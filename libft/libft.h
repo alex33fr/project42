850ebce76893bfdae57550ec89602980f5cffc72
alex33fr <a.privalov@hotmail.com>
2025-10-25 17:45:54 +0200

    create strjoin add strtrim

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: aprivalo <aprivalo@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 14:43:46 by aprivalo          #+#    #+#             */
-/*   Updated: 2025/10/25 14:43:50 by aprivalo         ###   ########.fr       */
+/*   Updated: 2025/10/25 17:25:20 by aprivalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,5 @@ int		ft_atoi(const char *s);
 void	*ft_calloc(size_t n, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 #endif
