af1f9af48828ae71f2d24a6343f07942fb080cf4
Aliexei Privalov <aprivalo@3D7.42angouleme.fr>
2025-10-31 17:47:00 +0100

    edit

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,29 +6,38 @@
 /*   By: aprivalo <aprivalo@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 18:49:22 by aprivalo          #+#    #+#             */
-/*   Updated: 2025/10/31 07:00:30 by aprivalo         ###   ########.fr       */
+/*   Updated: 2025/10/31 17:03:05 by aprivalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
-static int ft_vsnprintf(char *tab_p, size_t size, const char *s, va_list ap)
+void	ft_putstr_fd(char *s, int fd)
 {
 	size_t	i;
-	char	*str;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+static int	ft_vsnprintf(char *tab_p, size_t size, const char *s, va_list args)
+{
+	size_t	i;
 
 	i = 0;
 	while (*s && i + 1 < size)
 	{
 		if (*s == '%' && *(s + 1) == 's')
-		{
-			s += 2;
-			str = va_arg(ap, char *);
-			while (*str && i + 1 < size)
-				tab_p[i++] = *str++;
-		}
+			ft_putstr_fd(va_arg(args, char *), 1);
 		else
-			tab_p[i++] = *s++;
+		tab_p[i++] = *s++;
 	}
 	tab_p[i] = '\0';
 	return (i);
@@ -36,19 +45,38 @@ static int ft_vsnprintf(char *tab_p, size_t size, const char *s, va_list ap)
 
 int	ft_printf(const char *s, ...)
 {
-	int n;
-	size_t size;
-	char *tab_p;
-	va_list ap;
+	int	n;
+	size_t	size;
+	char	*tab_p;
+	va_list	ap;
 
 	size = 1024;
-	tab_p = ft_calloc(size, sizeof(char));
+	tab_p = malloc(size);
 	if (!tab_p)
-		return (0);
-	va_start(ap, s);
+		return (-1);
+		va_start(ap, s);
 	n = ft_vsnprintf(tab_p, size, s, ap);
 	va_end(ap);
 	write(1, tab_p, n);
 	free(tab_p);
-	return (n);
+    return (n);
 }
+
+int main(void){
+    int r1,r2;
+
+    r1=printf("1: Hello %s!\n","World");
+    r2=ft_printf("2: Hello %s!\n","World");
+    printf("result printf=%d | result ft_printf=%d\n\n",r1,r2);
+
+    r1=printf("3: Nombre=%d, Char=%c\n",42,'A');
+    r2=ft_printf("4: Nombre=%d, Char=%c\n",42,'A');
+    printf("result printf=%d | result ft_printf=%d\n\n",r1,r2);
+/*
+	r1=printf("5: Hex=%x, Str=%s\n",255,"test");
+    r2=ft_printf("6: Hex=%x, Str=%s\n",255,"test");
+    printf("result printf=%d | result ft_printf=%d\n",r1,r2);
+ */
+	return (0);
+}
+
