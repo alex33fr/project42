e441a49ddc02b3098d77d00ff258bd1ae64881b5
alex33fr <a.privalov@hotmail.com>
2025-11-04 12:15:56 +0100

    init get next line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aprivalo <aprivalo@student.42angouleme.fr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 12:11:53 by aprivalo          #+#    #+#             */
+/*   Updated: 2025/11/04 12:14:34 by aprivalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
