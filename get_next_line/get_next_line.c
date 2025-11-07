c76bb18a0c1730dcd478cff3c1c410d9d56301e8
alex33fr <a.privalov@hotmail.com>
2025-11-07 16:40:49 +0100

    on avance

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,125 @@
 /*   By: aprivalo <aprivalo@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 12:11:30 by aprivalo          #+#    #+#             */
-/*   Updated: 2025/11/04 12:15:32 by aprivalo         ###   ########.fr       */
+/*   Updated: 2025/11/07 16:39:56 by aprivalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*ft_strchr(const char *s, int c)
 {
+	char	cc;
+	char	*p;
 
+	cc = (char)c;
+	p = (char *)s;
+	while (*p)
+	{
+		if (*p == cc)
+			return (p);
+		p++;
+	}
+	if (cc == '\0')
+		return (p);
+	return (NULL);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len_src;
+
+	i = 0;
+	len_src = ft_strlen(src);
+	if (size > 0)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (len_src);
+}
+
+static	char	*ft_ret_line(char *s)
+{
+	ssize_t	i;
+	char	*str;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			break ;
+		i++;
+	}
+	str = ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+		{
+			str[i] = s[i];
+			i++;
+			break ;
+		}
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_upd_rest(char *rest)
+{
+	ssize_t	i;
+	char	*upd_rest;
+
+	i = 0;
+	while (rest[i])
+	{
+		if (rest[i] == '\n')
+		{
+			if (rest[i + 1])
+				upd_rest = ft_strdup(rest + i + 1);
+			else
+				upd_rest = NULL;
+			free(rest);
+			return (upd_rest);
+		}
+		i++;
+	}
+	free(rest);
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*rest;
+	char		buf[BUFFER_SIZE + 1];
+	ssize_t		tab_read;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	tab_read = 1;
+	while ((!rest || !ft_strchr(rest, '\n')) && tab_read > 0)
+	{
+		tab_read = read(fd, buf, BUFFER_SIZE);
+		if (tab_read <= 0)
+			break ;
+		buf[tab_read] = '\0';
+		rest = ft_strjoin(rest, buf);
+	}
+	if (tab_read == -1 || !rest || !*rest)
+	{
+		free(rest);
+		rest = NULL;
+		return (NULL);
+	}
+	line = ft_ret_line(rest);
+	rest = ft_upd_rest(rest);
+	return (line);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,8 +6,81 @@
 /*   By: aprivalo <aprivalo@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 12:11:44 by aprivalo          #+#    #+#             */
-/*   Updated: 2025/11/04 12:11:45 by aprivalo         ###   ########.fr       */
+/*   Updated: 2025/11/07 16:39:31 by aprivalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*p;
+
+	i = 0;
+	p = (unsigned char *)s;
+	while (i < n)
+	{
+		p[i] = (unsigned char)0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*tab;
+	size_t	t;
+
+	if (size != 0 && nmemb > ((size_t) - 1) / size)
+		return (NULL);
+	t = size * nmemb;
+	tab = malloc(t);
+	if (!tab)
+		return (NULL);
+	ft_bzero(tab, t);
+	return (tab);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*tab_join;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1)
+		s1 = ft_strdup("");
+	if (!s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	tab_join = ft_calloc(len_s1 + len_s2 + 1, sizeof(char));
+	if (!tab_join)
+		return (NULL);
+	ft_strlcpy(tab_join, s1, len_s1 + 1);
+	ft_strlcpy(tab_join + len_s1, s2, len_s2 + 1);
+	free(s1);
+	return (tab_join);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*tab_dup;
+	int		size;
+
+	size = ft_strlen(s);
+	tab_dup = malloc(sizeof(char) * (size + 1));
+	if (!tab_dup)
+		return (NULL);
+	ft_strlcpy(tab_dup, s, size + 1);
+	return (tab_dup);
+}
