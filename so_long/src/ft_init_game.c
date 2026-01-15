a6291a6178895e89a7bf77afaf491405b632ce9d
alex33fr <a.privalov@hotmail.com>
2026-01-15 19:05:06 +0100

    last updates

--- so_long/src/ft_init_game.c
+++ so_long/src/ft_init_game.c
@@ -6,7 +6,7 @@
 /*   By: aprivalo <aprivalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 16:43:10 by aprivalo          #+#    #+#             */
-/*   Updated: 2026/01/05 17:31:57 by aprivalo         ###   ########.fr       */
+/*   Updated: 2026/01/05 21:41:41 by aprivalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	ft_init_game(t_game *g)
 	int	w;
 	int	h;
 
-	g->img_wall = mlx_xpm_file_to_image(g->mlx, "textures/wall.xpm", &w, &h);
-	g->img_floor = mlx_xpm_file_to_image(g->mlx, "textures/floor.xpm", &w, &h);
-	g->img_coin = mlx_xpm_file_to_image(g->mlx, "textures/coin.xpm", &w, &h);
-	g->img_player = mlx_xpm_file_to_image(g->mlx, "textures/character_left.xpm", &w, &h);
-	g->img_exit = mlx_xpm_file_to_image(g->mlx, "textures/exit.xpm", &w, &h);
+	g->img_wall = mlx_xpm_file_to_image(g->mlx, "sprite/wall.xpm", &w, &h);
+	g->img_floor = mlx_xpm_file_to_image(g->mlx, "sprite/floor.xpm", &w, &h);
+	g->img_coin = mlx_xpm_file_to_image(g->mlx, "sprite/coin.xpm", &w, &h);
+	g->img_player = mlx_xpm_file_to_image(g->mlx, "sprite/p_left.xpm", &w, &h);
+	g->img_exit = mlx_xpm_file_to_image(g->mlx, "sprite/exit.xpm", &w, &h);
 }
--- so_long/src/ft_tracer_map.c
+++ so_long/src/ft_tracer_map.c
@@ -6,40 +6,45 @@
 /*   By: aprivalo <aprivalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 16:55:32 by aprivalo          #+#    #+#             */
-/*   Updated: 2026/01/05 17:32:19 by aprivalo         ###   ########.fr       */
+/*   Updated: 2026/01/05 21:31:24 by aprivalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_tracer_map(t_game *game)
+static void	ft_find_char(t_game *game, int x, int y)
 {
-    int	x;
-    int	y;
-
-
-    x = 0;
-    printf("start trace\n");
-    printf("size: w=%d h=%d\n", game->map_width, game->map_height);
-    while (x < game->map_height)
-    {
-        y = 0;
-        while (y < game->map_width)
-        {
-            if (game->map[x][y] == '1')
-	            mlx_put_image_to_window(game->mlx, game->win, game->img_wall, y * 32, x * 32);
-            else if (game->map[x][y] == '0')
-                mlx_put_image_to_window(game->mlx, game->win, game->img_floor, y * 32, x * 32);
-            else if (game->map[x][y] == 'P')
-                mlx_put_image_to_window(game->mlx, game->win, game->img_player, y * 32, x * 32);
-            else if (game->map[x][y] == 'E')
-                mlx_put_image_to_window(game->mlx, game->win, game->img_exit, y * 32, x * 32);
-            else if (game->map[x][y] == 'C')
-                mlx_put_image_to_window(game->mlx, game->win, game->img_coin, y * 32, x * 32);
-            y++;
-            printf("trace OK\n");
-        }
-        x++;
-    }
+	if (game->map[x][y] == '1')
+		mlx_put_image_to_window(game->mlx,
+			game->win, game->img_wall, y * 32, x * 32);
+	else if (game->map[x][y] == '0')
+		mlx_put_image_to_window(game->mlx,
+			game->win, game->img_floor, y * 32, x * 32);
+	else if (game->map[x][y] == 'P')
+		mlx_put_image_to_window(game->mlx,
+			game->win, game->img_player, y * 32, x * 32);
+	else if (game->map[x][y] == 'E')
+		mlx_put_image_to_window(game->mlx,
+			game->win, game->img_exit, y * 32, x * 32);
+	else if (game->map[x][y] == 'C')
+		mlx_put_image_to_window(game->mlx,
+			game->win, game->img_coin, y * 32, x * 32);
 }
 
+void	ft_tracer_map(t_game *game)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < game->map_height)
+	{
+		y = 0;
+		while (y < game->map_width)
+		{
+			ft_find_char(game, x, y);
+			y++;
+		}
+		x++;
+	}
+}
