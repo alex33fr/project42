25e5796d0c7769a12061d37d822c39e310831225
alex33fr <a.privalov@hotmail.com>
2026-03-16 08:14:02 +0100

    minimal philo

--- philosophers/includes/philo.h
+++ philosophers/includes/philo.h
@@ -6,15 +6,25 @@
 /*   By: aprivalo <aprivalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/01 16:14:09 by aprivalo          #+#    #+#             */
-/*   Updated: 2026/03/16 07:50:33 by aprivalo         ###   ########.fr       */
+/*   Updated: 2026/03/16 08:07:51 by aprivalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
-# include <sys/wait.h>
-
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
+typedef struct s_node
+{
+	int	value;
+	struct s_node	*next;
+}	t_node;
 typedef struct s_philo
 {
+	t_node *head;
 }	t_philo;
 #endif
