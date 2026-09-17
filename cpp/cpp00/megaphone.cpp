056ba300fc66cdf27038aa2cb148063dce0b6145
Aliexei Privalov <aprivalo@1C6.42angouleme.fr>
2026-09-17 14:50:03 +0200

    cpp

---
+++ cpp/cpp00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+    if(ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return (1);
+    }
+    else
+    {
+        for (int i = 1; i < ac; i++)
+        {
+            std::string tab = av[i];
+            char res;
+            for (size_t j = 0; j < tab.length(); j++)
+            {
+                res = toupper(tab[j]);
+                std::cout << res;
+            }
+        }
+        std::cout << "\n";
+    }
+    return (0);
+}
