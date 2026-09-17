a93caaaf0ce6d9c5b2dd65ea989fcd262dbfd502
Aliexei Privalov <aprivalo@1C6.42angouleme.fr>
2026-09-17 16:05:40 +0200

    cpp

---
+++ cpp/cpp01/PhoneBook.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aprivalo <aprivalo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/17 15:32:13 by aprivalo          #+#    #+#             */
+/*   Updated: 2026/09/17 16:05:22 by aprivalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_HPP
+# define PHONE_BOOK_HPP
+
+#include <string>
+#include <iostream>
+class PhoneBook
+{
+    private:
+    
+    public:
+    PhoneBook();
+    ~PhoneBook();
+};
+
+PhoneBook::PhoneBook()
+{
+}
+
+PhoneBook::~PhoneBook()
+{
+}
+
+class Contact
+{
+    private:
+    
+    public:
+    Contact();
+    ~Contact();
+};
+
+Contact::Contact()
+{
+}
+
+Contact::~Contact()
+{
+}
+# endif
