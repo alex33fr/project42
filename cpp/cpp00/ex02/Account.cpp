99c178d7344793661c9e9f8e02f5795178fbfed2
alex33fr <a.privalov@hotmail.com>
2026-09-25 10:23:14 +0200

    add account

--- cpp/cpp00/ex02/Account.cpp
+++ cpp/cpp00/ex02/Account.cpp
@@ -6,8 +6,20 @@
 /*   By: aprivalo <aprivalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/22 17:54:32 by aprivalo          #+#    #+#             */
-/*   Updated: 2026/09/22 18:03:50 by aprivalo         ###   ########.fr       */
+/*   Updated: 2026/09/23 08:00:39 by aprivalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <ctime>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+void _displayTimestamp(void)
+{
+    std::time_t now = std::time(NULL);
+    
+}
